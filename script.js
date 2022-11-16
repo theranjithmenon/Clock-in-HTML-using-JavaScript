47fb3497b749194aa3d41fa3097e86c9c4fc4c34
Ranjith <ranjithmenon112@gmail.com>
2022-11-16 17:26:09 +0530

    commit

--- script.js
+++ script.js
@@ -14,17 +14,41 @@ function ShowTime() {
     var month = time.getMonth();
     var whichDay = time.getDay();
 
+
+    if (hour > 12) {
+        hour = hour - 12;
+    }
+    if (hour < 10) {
+        document.getElementById('hour').innerHTML = "0" + hour;
+    }
+    else {
+        document.getElementById('hour').innerHTML = hour;
+    }
+
     if (hour >= 12) {
         session.innerHTML = 'PM';
+    }
+
+    if (minute < 10) {
+        document.getElementById('minute').innerHTML = "0" + minute;
+
+    }
+    else {
+        document.getElementById('minute').innerHTML = minute;
+    }
+
+
+    if (second < 10) {
+        document.getElementById('second').innerHTML = "0" + second;
+    }
+    else {
+        document.getElementById('second').innerHTML = second;
     }
 
     document.getElementById('day').innerHTML = day;
     document.getElementById('month').innerHTML = months[month];
     document.getElementById('whichDay').innerHTML = days[whichDay];
 
-    document.getElementById('hour').innerHTML = hour;
-    document.getElementById('minute').innerHTML = minute;
-    document.getElementById('second').innerHTML = second;
 }
 
 setInterval(ShowTime, 10);
